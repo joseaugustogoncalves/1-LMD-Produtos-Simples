0b9dc08a48b9b1a91afbb15931c415c5b82a63da
Jarg <jarg1976@gmail.com>
2026-01-29 11:58:31 +0000

    Criar espaçao para as respostas em solucao.sql

---
+++ solucao.sql
@@ -0,0 +1,9 @@
+-- Escrever Solução 1: Listar todos os produtos
+
+-- Escrever Solução 2: Obter cliente que realizou mais compras
+
+-- Escrever Solução 3: Listar vendas entre '2023-01-01' e '2023-01-10'
+
+-- Escrever Solução 4: Receita total (todas as vendas)
+
+-- Escrever Solução 5: Contar as unidades vendidas de cada produto
